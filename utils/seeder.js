9951ea75a6ea5e8a8b69100c2b55c512b0f22fb8
Ashley Stanbridge <ashley_stanbridge@yahoo.com>
2019-11-06 17:12:36 -0600

    Seeded section data

--- utils/seeder.js
+++ utils/seeder.js
@@ -1,33 +1,32 @@
 const Datastore = require('nedb') // set up a temporary (in memory) database
-//const developerData = require('../data/developers.json') // read in data file
+
 const studentData = require('../data/students.json') // read in data file
 const courseData = require('../data/courses.json')
 const instructorData = require('../data/instructors.json')
+const sectionData = require('../data/sections.json')
 // inject Express app to configure it - EVERYTHING in through argument list
 
 module.exports = (app) => {
   console.log('START data seeder.')
   const db = {} // empty object to hold all collections
 
- // db.developer = new Datastore() // new object property
   db.student = new Datastore() // new object property
   db.course = new Datastore() // new object property
   db.instructor = new Datastore() //new object property
+  db.section = new Datastore() //new object property
 
-  db.developer.loadDatabase() // call the loadDatabase method
   db.student.loadDatabase() // call the loadDatabase method
   db.course.loadDatabase() // call the loadDatabase method
   db.instructor.loadDatabase() //call the loadDatabase method
+  db.section.loadDatabase() //call the loadDatabase method
 
   // insert the sample data into our datastore
- // db.developer.insert(developerData)
   db.student.insert(studentData)
   db.course.insert(courseData)
   db.instructor.insert(instructorData)
+  db.section.insert(sectionData)
 
   // initialize app.locals (these objects are available to the controllers)
- // app.locals.student = db.developer.find(developerData)
-  // console.log(`${app.locals.student.query.length} developers seeded`)
 
   app.locals.student = db.student.find(studentData)
   console.log(`${app.locals.student.query.length} students seeded`)
@@ -35,8 +34,12 @@ module.exports = (app) => {
   app.locals.course = db.course.find(courseData)
   console.log(`${app.locals.course.query.length} courses seeded`)
 
-  app.locals.instructor = db.course.find(instructorData)
+  app.locals.instructor = db.instructor.find(instructorData)
   console.log(`${app.locals.course.query.length} instructors seeded`)
+
+  app.locals.section = db.section.find(sectionData)
+  console.log(`${app.locals.course.query.length} sections seeded`)
+
 
   console.log('END Data Seeder. Sample data read and verified.')
 }
