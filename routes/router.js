d0a855246005ab99ad981d3c62fcd3bfa34d86b8
Ashley Stanbridge <ashley_stanbridge@yahoo.com>
2019-11-07 12:36:16 -0600

    Updated app.js to include ejs engine. Changed designated index file to index.ejs in router.js

--- routes/router.js
+++ routes/router.js
@@ -15,12 +15,12 @@ const router = express.Router()
 // Manage top-level request first
 router.get('/', (req, res, next) => {
   console.log('Request to /')
-  res.sendFile('index.html')
+  res.sendFile('index.ejs')
 })
 
 router.get('/index', (req, res, next) => {
   console.log('Request to /index')
-  res.sendFile('index.html')
+  res.sendFile('index.ejs')
 })
 
 
