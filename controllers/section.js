3f0943a92f5267ff879027efe2edab2ae946e36a
Ashley Stanbridge <ashley_stanbridge@yahoo.com>
2019-11-13 12:18:48 -0600

    Changed section.js controller. Added Get and Post functionality

--- controllers/section.js
+++ controllers/section.js
@@ -1,90 +1,153 @@
 /**
-*  Section controller
-*  Handles requests related to section resources.
+*  Developer controller
+*  Handles requests related to developer resources.
 *
-* @author Group7
+* @author Denise Case <dcase@nwmissouri.edu>
 *
 */
 const express = require('express')
 const api = express.Router()
+const LOG = require('../utils/logger.js')
 const Model = require('../models/section.js')
-const find = require('lodash.find')
-const notfoundstring = 'Could not find section with id='
+const notfoundstring = 'Could not find course with id='
 
 // RESPOND WITH JSON DATA  --------------------------------------------
 
 // GET all JSON
 api.get('/findall', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  const data = req.app.locals.section.query
-  res.send(JSON.stringify(data))
+  LOG.info(`Handling /findall ${req}`)
+  Model.find({}, (err, data) => {
+    if (err) { return res.end('Error finding all') }
+    res.json(data)
+  })
 })
 
 // GET one JSON by ID
 api.get('/findone/:id', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
+  LOG.info(`Handling /findone ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.section.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring + id) }
-  res.send(JSON.stringify(item))
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(`notfoundstring ${id}`) }
+    res.json(results[0])
+  })
 })
 
 // RESPOND WITH VIEWS  --------------------------------------------
 
-// GET to this controller base URI (the default)
+// GET /
 api.get('/', (req, res) => {
-  res.render('index.html', {
-    developers: req.app.locals.developers.query
+  LOG.info(`Handling GET / ${req}`)
+  Model.find({}, (err, data) => {
+    if (err) { return res.end('Error') }
+    res.locals.sections = data
+    res.render('section/index.ejs')
   })
 })
 
 // GET create
 api.get('/create', (req, res) => {
-  res.render('section/create', {
-    developers: req.app.locals.developers.query,
-    developer: new Model()
+  LOG.info(`Handling GET /create ${req}`)
+  Model.find({}, (err, data) => {
+    if (err) { return res.end('error on create') }
+    res.locals.sections = data
+    res.locals.section = new Model()
+    res.render('section/create')
   })
 })
 
 // GET /delete/:id
 api.get('/delete/:id', (req, res) => {
+  LOG.info(`Handling GET /delete/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.developers.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring + id) }
-  res.render('section/delete', {
-    developer: item
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR ${JSON.stringify(results)}`)
+    res.locals.section = results[0]
+    return res.render('section/delete.ejs')
   })
 })
 
 // GET /details/:id
 api.get('/details/:id', (req, res) => {
+  LOG.info(`Handling GET /details/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.developers.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring + id) }
-  res.render('section/details', {
-    developer: item
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR ${JSON.stringify(results)}`)
+    res.locals.section = results[0]
+    return res.render('section/details.ejs')
   })
 })
 
 // GET one
 api.get('/edit/:id', (req, res) => {
+  LOG.info(`Handling GET /edit/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.developers.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring + id) }
-  res.render('section/edit', {
-    developer: item
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR${JSON.stringify(results)}`)
+    res.locals.section = results[0]
+    return res.render('section/edit.ejs')
   })
 })
 
+// HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
 
-// later
+// POST /save
+api.post('/save', (req, res) => {
+  LOG.info(`Handling POST ${req}`)
+  LOG.debug(JSON.stringify(req.body))
+  const item = new Model()
+  LOG.info(`NEW ID ${req.body._id}`)
+  item._id = parseInt(req.body._id)
+  item.sectionNumber = req.body.sectionNumber
+  item.days = req.body.days
+  item.startTime = req.body.startTime
+  item.roomNumber = req.body.roomNumber
+  item.instructorId = req.body.instructorId
+  item.courseId = req.body.courseId
+  item.save((err) => {
+    if (err) { return res.end('ERROR: item could not be saved') }
+    LOG.info(`SAVING NEW item ${JSON.stringify(item)}`)
+    return res.redirect('/section')
+  })
+})
 
-// RESPOND WITH DATA MODIFICATIONS  -------------------------------
+// POST save with id
+api.post('/save/:id', (req, res) => {
+  LOG.info(`Handling SAVE request ${req}`)
+  const id = parseInt(req.params.id)
+  LOG.info(`Handling SAVING ID=${id}`)
+  Model.updateOne({ _id: id },
+    { // use mongoose field update operator $set
+      $set: {
+        sectionNumber: req.body.sectionNumber,
+        days: req.body.days,
+        startTime: req.body.startTime,
+        roomNumber: req.body.roomNumber,
+        instructorId: req.body.instructorId,
+        courseId: req.body.courseId,
+      }
+    },
+    (err, item) => {
+      if (err) { return res.end(notfoundstring) }
+      LOG.info(`ORIGINAL VALUES ${JSON.stringify(item)}`)
+      LOG.info(`UPDATED VALUES: ${JSON.stringify(req.body)}`)
+      LOG.info(`SAVING UPDATED item ${JSON.stringify(item)}`)
+      return res.redirect('/section')
+    })
+})
 
-// later
+// DELETE id (uses HTML5 form method POST)
+api.post('/delete/:id', (req, res) => {
+  LOG.info(`Handling DELETE request ${req}`)
+  const id = parseInt(req.params.id)
+  LOG.info(`Handling REMOVING ID=${id}`)
+  Model.remove({ _id: id }).setOptions({ single: true }).exec((err, deleted) => {
+    if (err) { return res.end(notfoundstring) }
+    console.log(`Permanently deleted item ${JSON.stringify(deleted)}`)
+    return res.redirect('/section')
+  })
+})
 
 module.exports = api
